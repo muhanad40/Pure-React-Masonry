ed07ce2ac2e1b3df33792c220d96fb612aac474e
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2018-06-10 16:41:56 +0100

    Mount App component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-// entry file
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('masonry-mount')
+);
