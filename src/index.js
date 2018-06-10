707882eda37d5261991c6dc15d591072407563ef
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2018-06-10 17:24:41 +0100

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
-import App from './App';
+import ReactMasonry from './ReactMasonry';
 
 ReactDOM.render(
-  <App />,
+  <ReactMasonry />,
   document.getElementById('masonry-mount')
 );
